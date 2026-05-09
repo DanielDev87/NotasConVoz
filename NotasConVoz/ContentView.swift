5c70ad6f89c0709b548c72fa74e91079c592ec01
DaniDev87 <danielhymtj@icloud.com>
2026-05-08 21:31:05 -0500

    Initial Commit

---
+++ NotasConVoz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NotasConVoz
+//
+//  Created by Daniel Felipe Agudelo Molina on 8/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NotasConVoz/NotasConVozApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotasConVozApp.swift
+//  NotasConVoz
+//
+//  Created by Daniel Felipe Agudelo Molina on 8/05/26.
+//
+
+import SwiftUI
+
+@main
+struct NotasConVozApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
